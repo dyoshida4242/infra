cf631b39cdfc80a0f37ad242a718b7101a0ab017
dyoshida4242 <daiy734111@outlook.jp>
2021-04-07 23:58:41 +0900

    VPC作成

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,11 @@
-resource "aws_instance" "example" {
-  ami = "ami-0f9ae750e8274075b"
-  instance_type = "t3.micro"
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "test-vpc"
+  }
 }
